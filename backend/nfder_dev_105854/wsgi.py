43081cc1eb22c8c790f0d18d81a9725a92dbdfa1
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 18:02:47 +0000

    Initial commit.

---
+++ backend/nfder_dev_105854/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nfder_dev_105854 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nfder_dev_105854.settings')
+
+application = get_wsgi_application()
