78131e1c19aa8b66afdc25b7934a810f3d9062dc
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-31 18:08:24 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Suv1
+admin.site.register(Suv1)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Suv1(models.Model):
+    'Generated Model'
+    bcncj = models.BigIntegerField()
+    bcdh = models.BigIntegerField()
